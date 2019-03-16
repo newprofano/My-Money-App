0fddaac8fa94a79cfb6c7b5d71dcf43c77f4c646
Christian <newera.cabal@hotmail.com>
2019-03-16 16:09:03 -0300

    component list

--- frontend/src/billingCycles/billingCycles.jsx
+++ frontend/src/billingCycles/billingCycles.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
@@ -6,8 +8,17 @@ import Tabs from '../common/tab/tabs';
 import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
 import TabHeader from '../common/tab/tabHeader';
+import TabContent from '../common/tab/tabContent';
+import { selectTab, showTabs } from '../common/tab/tabActions';
+import { create } from './billingCyclesActions';
+import List from './billingCyclesList';
+import Form from './billingCyclesForm';
 
 class BillingCycle extends Component {
+  componentWillMount() {
+    this.props.selectTab('tabList');
+    this.props.showTabs('tabList', 'tabCreate');
+  }
   render() {
     return (
       <div>
@@ -20,7 +31,20 @@ class BillingCycle extends Component {
               <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
               <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
             </TabsHeader>
-            <TabsContent />
+            <TabsContent>
+              <TabContent id="tabList">
+                <List />
+              </TabContent>
+              <TabContent id="tabCreate">
+                <Form onSubmit={this.props.create}/>
+              </TabContent>
+              <TabContent id="tabUpdate">
+                <h1>Alterar</h1>
+              </TabContent>
+              <TabContent id="tabDelete">
+                <h1>Excluir</h1>
+              </TabContent>
+            </TabsContent>
           </Tabs>
         </Content>
       </div>
@@ -28,4 +52,9 @@ class BillingCycle extends Component {
   }
 }
 
-export default BillingCycle;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ selectTab, showTabs, create }, dispatch);
+export default connect(
+  null,
+  mapDispatchToProps
+)(BillingCycle);
